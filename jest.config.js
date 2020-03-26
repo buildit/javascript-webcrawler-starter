6ba71ef462801a72f0b7d988f42b972611e09a66
Chris Ashurst <chris@telestatic.net>
2020-03-26 11:37:25 -0400

    Initial import

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  clearMocks: true,
+  collectCoverage: false,
+  collectCoverageFrom: [
+    '!**/node_modules/**',
+    '!**/index.js',
+    'src/**/*.js',
+  ],
+  coverageDirectory: 'coverage',
+  preset: 'jest-puppeteer',
+  reporters: ['default'],
+  rootDir: process.cwd(),
+  roots: [
+    '<rootDir>/src',
+  ],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.js$',
+  verbose: false,
+}
