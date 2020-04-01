613f445aa72617ed3b59a098ac82ecfca07b141b
Chris Ashurst <chris@telestatic.net>
2020-04-01 16:00:15 -0400

    refactored helpers into stubs and dummies

--- src/app/helpers.js
+++ src/app/helpers.js
@@ -1,4 +1,12 @@
-import { $form, $domain, $submit } from './elements'
+/**
+ * Returns a reference to the webcrawler form
+ */
+const $form = document.querySelector('form[name=crawler]')
+
+/**
+ * Returns a reference to the domain text input from the context of `$form`
+ */
+const $domain = $form.querySelector('input[name=domain]')
 
 /**
  * Application entrypoint
@@ -8,75 +16,36 @@ import { $form, $domain, $submit } from './elements'
  */
 export const setupCrawlerApplication = (crawler) => {
   $form.onsubmit = handleSubmit(crawler)
-  focusDomain()
 }
 
 /**
  * Form submission handler. It is bound at runtime through
  * `setupCrawlerApplication`.
  * 
- * Handles small UI tasks before and after the crawler function is called.
+ * This returned function should perform UI tasks such as toggling
+ * loading/busy state, preventing invalid input, etc
  * 
  * @param {*} crawler This function will be called when the crawler
  * form is submitted. It will be passed the value of the domain text input.
+ * 
+ * @returns {function} an onsubmit event handler function
  */
 const handleSubmit = crawler => async (event) => {
   event.preventDefault()
 
-  blurDomain()
-  setLoading()
-  unsetResults()
+  // Pre-Submit events/triggers here
 
   const data = await crawler($domain.value)
 
+  // Post-Submit events/triggers here
+
   setResults(data)
-  unsetLoading()
-  focusDomain()
 }
 
 /**
- * Focuses the domain text input
- */
-export const focusDomain = () => $domain.focus()
-
-/**
- * Removes focus from the domain text input
- */
-export const blurDomain = () => $domain.blur()
-
-/**
- * Toggles the presence of both the `is-loading` css class and
- * the `disabled` attribute on the submit button element
- * @param {boolean} force 
- */
-const toggleLoading = (force) => {
-  $submit.classList.toggle('is-loading', force)
-  $submit.toggleAttribute('disabled', force)
-}
-
-/**
- * Calls `toggleLoading(true)`
- * @see toggleLoading
- */
-export const setLoading = () => toggleLoading(true)
-
-/**
- * Calls `toggleLoading(false)`
- * @see toggleLoading
- */
-export const unsetLoading = () => toggleLoading(false)
-
-/**
- * Returns a reference to the `pre` element if it exists
- */
-const getPreElement = () => $form.querySelector('pre')
-
-/**
- * Creates a new `pre` element with some pre-defined css classes
- * and attributes before being appended as a child of the crawler form
- * @param {*} data Some kind of data. It is sanitized using
- * `JSON.stringify` before being set onto the pre element via
- * the `textContent` property
+ * Do something with result data returned by your crawler function
+ * 
+ * @param {*} data 
  */
 export const setResults = (data) => {
   const $pre = document.createElement('pre')
@@ -84,8 +53,3 @@ export const setResults = (data) => {
   $pre.textContent = JSON.stringify(data, null, 2)
   $form.appendChild($pre)
 }
-
-/**
- * Removes the results `pre` element from the crawler form if it exists.
- */
-export const unsetResults = () => $form.contains(getPreElement()) && $form.removeChild(getPreElement())
--- src/crawler/crawl-domain.js
+++ src/crawler/crawl-domain.js
@@ -10,6 +10,9 @@ const wait = async duration => new Promise(resolve => setTimeout(resolve, durati
  * This is here purely as a reference for both expected input and output
  * of this exercise.
  * 
+ * This function should return data as defined in `src/model.js` and the
+ * markup of the page found at `domain` for embedding into the UI.
+ * 
  * Note that this function uses async/await.
  * @param {string} domain the domain/URL to start crawling from
  */
